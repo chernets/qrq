146389a0204b2d31f37211bf3e8c31477483d8bd
Yura Tronyak <tronyakyura@gmail.com>
2015-10-13 23:29:22 +0300

    addnews commit

--- client/app/addnews/addnews.controller.js
+++ client/app/addnews/addnews.controller.js
@@ -2,5 +2,16 @@
 
 angular.module('qrqApp')
   .controller('addnewsCtrl', function($scope, $http, $rootScope) {
-	  
+	  $scope.mainform = {};
+		
+      $scope.update = function(newsadd) {
+        $scope.mainform = angular.copy(newsadd);
+		console.log($scope.newsadd);
+      };
+
+      $scope.reset = function() {
+        $scope.newsadd = angular.copy($scope.mainform);
+      };
+
+      $scope.reset();
   });
